93a3a6a9f7c7de2c9c75ea37b03c32ac227662b4
JuliaKivenko <kivenko.julia@yahoo.com>
2022-05-30 22:14:25 +0200

    Phoenix
    Implemented phoenix model

--- Assets/Scripts/Player/playerVisualsRotator.cs
+++ Assets/Scripts/Player/playerVisualsRotator.cs
@@ -4,29 +4,10 @@ using UnityEngine;
 
 public class playerVisualsRotator : MonoBehaviour
 {
-    [SerializeField] float rotateStrength = 50f;
-
-
-
+    [SerializeField] float rotateStrength = 25f;
 
     void Update()
     {
-
-        /*if (PlayerController.sharedInstance.GetNormalizedVelocity().y >= 0)
-        {
-            transform.Rotate(Vector3.forward * rotateStrength * 5 * Time.deltaTime * PlayerController.sharedInstance.GetNormalizedVelocity().y);
-
-        }
-        else
-        {
-            transform.Rotate(Vector3.forward * rotateStrength * 2 * Time.deltaTime * PlayerController.sharedInstance.GetNormalizedVelocity().y);
-        }
-
-        float currentZRotation = transform.eulerAngles.z;
-        //currentZRotation = (currentZRotation > 180) ? currentZRotation - 360 : currentZRotation;
-        currentZRotation = Mathf.Clamp(currentZRotation, ROTATION_MIN, ROTATION_MAX);
-        transform.rotation = Quaternion.Euler(0, 0, currentZRotation);*/
-
         transform.localEulerAngles = new Vector3(0, 0, PlayerController.sharedInstance.GetVelocity().y * rotateStrength);
     }
 
